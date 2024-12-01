cdffdd93c50176f83c8f3d3ad8bf684e52fe57e7
AmandaOdawa <amandaodawa1@gmail.com>
2024-12-01 14:31:07 +0300

    challenge3 start

---
+++ challenge3.js
@@ -0,0 +1,43 @@
+function calculateSalary (basicSalary, benefits) {
+    const totalSalary = basicSalary + benefits;
+
+    //Tax categories from 1st July 2024 (KRA)
+    let tax = 0;
+    if (totalSalary <= 24000) {
+        tax = totalSalary * 0.1 //10% tax rate
+    }
+    else if (totalSalary <= 32333) {
+        tax = totalSalary * 0.25 //25%
+    }
+    else if (totalSalary <= 500000) {
+        tax = totalSalary * 0.3 //30%
+    }
+    else if (totalSalary <= 800000) {
+        tax = totalSalary * 0.325 //32.5%
+    }
+    else {
+        tax = totalSalary * 0.35 //35%
+    }
+
+
+    //NHIF deductions as of 30 September 2024
+    let nhif = 0
+    if (totalSalary <= 5999) nhif = 150
+    else if (totalSalary <= 7999) nhif = 300
+    else if (totalSalary <= 11999) nhif = 400
+    else if (totalSalary <= 14999) nhif = 500
+    else if (totalSalary <= 19999) nhif = 600
+    else if (totalSalary <= 24999) nhif = 750
+    else if (totalSalary <= 29999) nhif = 850
+    else if (totalSalary <= 34999) nhif = 900
+    else if (totalSalary <= 39999) nhif = 950
+    else if (totalSalary <= 44999) nhif = 1000
+    else if (totalSalary <= 49999) nhif = 1100
+    else if (totalSalary <= 59999) nhif = 1200
+    else if (totalSalary <= 69999) nhif = 1300
+    else if (totalSalary <= 79999) nhif = 1400
+    else if (totalSalary <= 89999) nhif = 1500
+    else if (totalSalary <= 99999) nhif = 1600
+    else nhif = 1700
+}
+
